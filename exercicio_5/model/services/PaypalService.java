43ba96b5d0344bc64e5472f2e98362119aad6488
João pedro Floriano <joaopflori23@gmail.com>
2026-01-29 19:03:34 -0300

    Adiciona override em PaypalService

--- exercicio_5/model/services/PaypalService.java
+++ exercicio_5/model/services/PaypalService.java
@@ -2,9 +2,11 @@ package exercicio_5.model.services;
 
 public class PaypalService implements OnlinePaymentService{
 
+    @Override
     public Double paymentFee(Double amount){
         return amount * 0.02;
     }
+    @Override
     public Double interest(Double amount, Integer months){
         return amount * 0.01 * months;
     }
